dae8ab37017a171e73ab7ce0bad4c714b17e1d9a
Vijay Pal <vijayroykargwal@gmail.com>
2019-07-22 14:56:55 +0530

    Add files via upload

---
+++ DBMS/RDBMS-Day3-A-20-33/Solution/SQL/Solution_33.sql
@@ -0,0 +1,3 @@
+ALTER TABLE EmpDetails ADD (CurrentSalary INTEGER,increased salary INTEGER,incremented amount INTEGER)
+UPDATE EmpDetails SET (CurrentSalary = Salary,Increased Salary=1.1*Salary,Incremented Amount=0.1*Salary)
+SELECT empid, CurrentSalary, increased salary, incremented amount from EmpDetails
